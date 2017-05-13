b2b78d21acb37bd82b421beefdb51c8e7af9d1b1
MarianRod <marianorod.msk@gmail.com>
2017-05-12 21:28:34 -0300

    subiendo puntos practica 1

---
+++ practica_1/ej2p2.py
@@ -0,0 +1,2 @@
+car = raw_input("Ingrese un caracter: ")
+print "El valor ASCII de %s es %s" % (car, ord(car))
---
+++ practica_1/ej5p3.py
@@ -0,0 +1,4 @@
+n = input("ingrese un ano: ")
+if (n%4==0):
+    if (n%!=100):
+        print "el ano es bisiesto"
