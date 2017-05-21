5d1422e94d68349c563fbe1f953c0c7fbb9b5654
MarianRod <marianorod.msk@gmail.com>
2017-05-21 17:42:31 -0300

    cambiando nombre de carpeta dentro del repositorio

---
+++ practica_2/ej4p2.py
@@ -0,0 +1,11 @@
+n = input("Ingrese un numero: ")
+
+while (n != 0):
+
+    if (n>0):
+        print "el numero es positivo"
+    if (n<0):
+        print "el numero es negativo"
+    elif  (n==0):
+        print "el numero es 0"
+    n = input("Ingrese un numero: ")
---
+++ practica_2/ejp3.py
@@ -0,0 +1,4 @@
+lado = raw_input("Ingrese un valor para el lado de un cuadrado: ")
+p=lado*4
+a=lado*2
+print "el valor del perimetro es %s y el valor del area es %s" %(p,a)
